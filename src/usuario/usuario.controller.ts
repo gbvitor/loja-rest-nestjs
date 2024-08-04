f65e83d2988ba997982524aa2109399b214cede9
Vitor Gabriel <sgvitor@outlook.com.br>
2024-08-03 21:05:03 -0300

    feat: Implementation of the user DTO and entity

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,6 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CriaUsuarioDTO } from './dto/CriaUsuario.dto';
 import { UsuarioRepository } from './usuario.repository';
+import { UsuarioEntity } from './usuario.entity';
+import { v4 as uuid } from 'uuid';
+import { ListaUsuarioDTO } from './dto/ListaUsuario.dto';
 
 @Controller('/usuarios')
 export class UsuarioController {
@@ -8,12 +11,24 @@ export class UsuarioController {
 
   @Post()
   async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO) {
-    this.usuarioRepository.salvar(dadosDoUsuario);
-    return dadosDoUsuario;
+    const usuarioEntity = new UsuarioEntity();
+    usuarioEntity.nome = dadosDoUsuario.nome;
+    usuarioEntity.email = dadosDoUsuario.email;
+    usuarioEntity.senha = dadosDoUsuario.senha;
+    usuarioEntity.id = uuid();
+    this.usuarioRepository.salvar(usuarioEntity);
+    return {
+      usuario: new ListaUsuarioDTO(usuarioEntity.id, usuarioEntity.nome),
+      message: 'Usuário criado com sucesso!',
+    };
   }
 
   @Get()
   async listUsuarios() {
-    return this.usuarioRepository.listar();
+    const usuariosSalvos = await this.usuarioRepository.listar();
+    const usuariosLista = usuariosSalvos.map(
+      (u) => new ListaUsuarioDTO(u.id, u.nome),
+    );
+    return usuariosLista;
   }
 }
